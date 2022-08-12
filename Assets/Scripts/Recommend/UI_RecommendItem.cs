844494b652d56fef53a90c67b4c146193c22277d
GodHongIl <adunstudio@daum.net>
2022-08-12 15:28:19 +0900

    끝

--- Assets/Scripts/Recommend/UI_RecommendItem.cs
+++ Assets/Scripts/Recommend/UI_RecommendItem.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class UI_RecommendItem : MonoBehaviour
 {
     private RecommendResponseData m_Data;
 
-    public Image m_IconImage;
+    public RawImage m_IconImage;
     public TextMeshProUGUI m_NameText;
     public TextMeshProUGUI m_PriceText;
     public TextMeshProUGUI m_DescriptionText;
@@ -26,5 +27,26 @@ public class UI_RecommendItem : MonoBehaviour
         m_NameText.text = m_Data.Title;
         m_PriceText.text = $"{Int32.Parse(m_Data.Price):N0}₩";
         m_DescriptionText.text = $"{m_Data.Category} / {m_Data.Color} / {m_Data.Brand}";
+
+        StartCoroutine(DownloadImage(m_Data.Image));
+    }
+
+    IEnumerator DownloadImage(string MediaUrl)
+    {   
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            m_IconImage.texture = ((DownloadHandlerTexture) request.downloadHandler).texture;
+        }
+    } 
+    
+    public void OnClickBuyButton()
+    {
+        Application.OpenURL(m_Data.Link);
     }
 }
