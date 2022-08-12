952677d73d7164387e76831b34496d185a94124b
JH Park <juliajuhee222@gmail.com>
2022-08-12 13:31:25 +0900

    Revert "Revert "Merge branch 'main' of https://github.com/juliajh/8_Project""
    This reverts commit 8ae817c385567f3424f3b60238fda6a421dd79ad.

---
+++ Assets/Scripts/Furniture/Stand/Stand.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stand 
+{
+    public readonly TBL_STAND Data;
+
+    public Stand(TBL_STAND data)
+    {
+        Data = data;
+    }
+}
