16b6456ca93481d8f083f6a37a2681e13f3e3e04
leebach98 <44629665+leebach98@users.noreply.github.com>
2022-08-15 17:25:44 +0900

    에드포스 고치기 오브젝트 에드포스 추가

--- Assets/Scripts/SM/InteriorObject/InteriorObject.cs
+++ Assets/Scripts/SM/InteriorObject/InteriorObject.cs
@@ -41,13 +41,9 @@ public abstract class InteriorObject : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log("Down");      
-
         isSelected = true;
-
         m_Material.EnableKeyword("OUTBASE_ON");
-
-        
-       
+        gameObject.tag = "Furniture";
 
     }
 
@@ -57,7 +53,7 @@ public abstract class InteriorObject : MonoBehaviour
 
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        worldPosition.x = Mathf.Clamp(worldPosition.x, (float)-4.39, (float)4.39);
+        worldPosition.x = Mathf.Clamp(worldPosition.x, (float)-4.39, (float)4.61);
         worldPosition.y = Mathf.Clamp(worldPosition.y, (float)-1.3, (float)3);
         worldPosition.z = 0;
 
@@ -73,21 +69,65 @@ public abstract class InteriorObject : MonoBehaviour
 
     }
 
-
+    
     public void Rortate()
     {
         print("Rotate");
-       
-        
-
-
         direction = (Direction)((int)direction + 1);
-
- 
 
        
     }
+    BoxCollider2D furnitureCollider;
+    Rigidbody2D furnitureRigidBody;
 
-  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("LeftWall"))
+        {
+            NuckBack(new Vector2(10, 0));   
+        }
+
+        if (collision.gameObject.CompareTag("RightWall"))
+        {
+            NuckBack(new Vector2(-10, 0));
+        }
+
+        if (collision.gameObject.CompareTag("Furniture")&&isSelected == true) 
+        {
+            NuckBack(new Vector3(-10, 0));
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("RightWall") || collision.gameObject.CompareTag("LeftWall") || collision.gameObject.CompareTag("Furniture"))
+        {
+            StartCoroutine(StopRigidBody());
+        }
+    }
+    
+    IEnumerator StopRigidBody()
+    {
+        yield return new WaitForSeconds(0.2f);
+        furnitureRigidBody.velocity = new Vector2(0, 0);
+        furnitureCollider.isTrigger = false;
+        furnitureRigidBody.bodyType = RigidbodyType2D.Kinematic;
+
+    }
+
+
+    void NuckBack(Vector2 dir) 
+    {
+        print("NuckBack");
+        //print(col);
+        furnitureCollider = gameObject.GetComponent<BoxCollider2D>();
+        furnitureRigidBody = gameObject.GetComponent<Rigidbody2D>();
+        //furnitureCollider.isTrigger = false;
+
+        furnitureRigidBody.gravityScale = 0f;
+        furnitureRigidBody.bodyType = RigidbodyType2D.Dynamic;
+        furnitureRigidBody.AddForce(dir);
+        furnitureRigidBody.velocity = new Vector2(0.5f, 0);
+    }
+
 
 }
