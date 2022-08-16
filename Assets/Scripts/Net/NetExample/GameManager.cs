0355cfbe7293bde2590b032a2cbca8fb6dfc1c24
leebach98 <44629665+leebach98@users.noreply.github.com>
2022-08-16 18:02:03 +0900

    fadein

---
+++ Assets/Scripts/Net/NetExample/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text saveText;
+    public Color color;
+    private void Start()
+    {
+        color = saveText.GetComponent<Color>();
+    }
+    void Update()
+    {
+        StartCoroutine(AutoSave());   
+    }
+
+
+
+    IEnumerator AutoSave() 
+    {
+        yield return new WaitForSeconds(3f);
+        //FurnitureManager.Instance.Save();
+
+
+        color.r = Mathf.Lerp(0,255,2f);
+        Color.Lerp(saveText.color.r, saveText.color, color.r);
+        saveText.color = color;
+
+        //saveText.SetActive(true);
+    }
+}
--- Assets/Scripts/SM/InteriorObject/SofaObject.cs
+++ Assets/Scripts/SM/InteriorObject/SofaObject.cs
@@ -23,6 +23,38 @@ public class SofaObject : InteriorObject
         m_SpriteRenderer.sprite = m_Sofa.Data.FrontImage;
     }
 
+
+    public override void RotationObject()
+    {
+        switch (direction)
+        {
+            case Direction.Front:
+                {
+                    //transform.Rotate(0, 0, 90);
+                    direction = Direction.Right;
+                    m_SpriteRenderer.sprite = m_Sofa.Data.RightImage;
+                    //StartCoroutine(Spin());
+                    //Rortate();
+                }
+                break;
+            case Direction.Right:
+                {
+                    //transform.Rotate(0, 0, 90);
+                    direction = Direction.Left;
+                    m_SpriteRenderer.sprite = m_Sofa.Data.LeftImage;
+                }
+                break;
+            case Direction.Left:
+                {
+                    //transform.Rotate(0, 0, 90);
+                    direction = Direction.Front;
+                    m_SpriteRenderer.sprite = m_Sofa.Data.FrontImage;
+                    //Rortate();
+                }
+                break;
+        }
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && isSelected == true)
