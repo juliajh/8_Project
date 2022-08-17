c4fde2bc12176e313b59aee1dd645a6b38bf1f5a
chowoohyun <skywoohy93@gmail.com>
2022-08-17 21:04:10 +0900

    ddddd

--- AI/corr.py
+++ AI/corr.py
@@ -15,7 +15,7 @@ from nltk import Text
 
 okt = Okt()
 
-with open('craw_text.txt', 'r', encoding='utf-8') as file:
+with open('오늘의집 MZ.txt', 'r', encoding='utf-8') as file:
     x = file.read()
 
 tmp_list = okt.pos(x, norm=True, stem=True)
@@ -23,27 +23,27 @@ myList_filter = [x for x, y in tmp_list if y in ['Noun']]
 count = Counter(myList_filter)
 print(count)
 
-# words = (dict(count.most_common()))
-# Okt = Text(myList_filter, name="Okt")
-# wordInfo = dict()
-# for tags, counts in Okt.vocab().most_common(20):
-#     if (len(str(tags)) > 1):
-#         wordInfo[tags] = counts
-# values = sorted(wordInfo.values(), reverse=True)
-# keys = sorted(wordInfo, key=wordInfo.get, reverse=True)
+words = (dict(count.most_common()))
+Okt = Text(myList_filter, name="Okt")
+wordInfo = dict()
+for tags, counts in Okt.vocab().most_common(20):
+    if (len(str(tags)) > 1):
+        wordInfo[tags] = counts
+values = sorted(wordInfo.values(), reverse=True)
+keys = sorted(wordInfo, key=wordInfo.get, reverse=True)
 
-# #오류 방지 폰트 설정
-# font_location = "malgun.ttf"
-# font_name = font_manager.FontProperties(fname=font_location).get_name()
-# rc('font', family=font_name)
-#
-# # plt 그래프
-# plt.bar(range(len(wordInfo)), values, align='center')
-# plt.xticks(range(len(wordInfo)), list(keys), rotation='70')
-# plt.show()
-#
-# # wordcloud
-# wc = WordCloud(width = 1000, height = 600, background_color="white", font_path=font_location, max_words=50)
-# plt.imshow(wc.generate_from_frequencies(Okt.vocab()))
-# plt.axis("off")
-# plt.show()
+#오류 방지 폰트 설정
+font_location = "malgun.ttf"
+font_name = font_manager.FontProperties(fname=font_location).get_name()
+rc('font', family=font_name)
+
+# plt 그래프
+plt.bar(range(len(wordInfo)), values, align='center')
+plt.xticks(range(len(wordInfo)), list(keys), rotation='70')
+plt.show()
+
+# wordcloud
+wc = WordCloud(width = 1000, height = 600, background_color="white", font_path=font_location, max_words=50)
+plt.imshow(wc.generate_from_frequencies(Okt.vocab()))
+plt.axis("off")
+plt.show()
