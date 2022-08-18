62e86e1bf1548d903a55cb039910b280e65a2175
chowoohyun <skywoohy93@gmail.com>
2022-08-18 13:26:10 +0900

    add wordcloud
    craw data wordcloud,
    app review craw data wordcloud

--- AI/scraping_app_review.py
+++ AI/scraping_app_review.py
@@ -1,12 +1,18 @@
 from google_play_scraper import Sort, reviews_all
+import pandas as pd
+import numpy as np
 
 result = reviews_all(
-    'com.iCan Design lab.roomplanner',
+    'com.icandesignapp.all',
     sleep_milliseconds=0, # defaults to 0
     lang='kr', # defaults to 'en'
     country='kr', # defaults to 'us'
+    count= 3,
     sort=Sort.MOST_RELEVANT, # defaults to Sort.MOST_RELEVANT
-    filter_score_with=5 # defaults to None(means all score)
+    filter_score_with= 0 # defaults to None(means all score)
 )
 
-print(result)
+df_busu = pd.DataFrame(np.array(result), columns=['review'])
+df_busu = df_busu.join(pd.DataFrame(df_busu.pop('review').tolist()))
+
+df_busu.head()
