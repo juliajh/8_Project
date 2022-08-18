47a389a038d01ab2cde953b04ecf8067f9083aab
leebach98 <44629665+leebach98@users.noreply.github.com>
2022-08-19 04:31:03 +0900

    dtd

--- Assets/Scripts/Carrot/UI_CarrotWrite.cs
+++ Assets/Scripts/Carrot/UI_CarrotWrite.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class UI_CarrotWrite : MonoBehaviour
 {
+    private void Awake()
+    {
+
+        gameObject.SetActive(false);
+    }
     // Start is called before the first frame update
     void Start()
     {
